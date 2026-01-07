3b483072d6bafde600a1b5548a7c063783d751dc
Akhlaq Ahmed <ahmedakhlaq009@gmail.com>
2026-01-08 01:48:00 +0500

    Swagger

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ import type MessageResponse from "./interfaces/message-response.js";
 
 import api from "./api/index.js";
 import * as middlewares from "./middlewares.js";
-// import { setupSwagger } from "./swagger";
 
 const app = express();
 
@@ -23,7 +22,7 @@ app.get<object, MessageResponse>("/", (req, res) => {
 });
 
 app.use("/api/v1", api);
-// setupSwagger(app);
+
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
