c648eab12ec779c28d0a1aad3f809f7d95ad4a9b
Akhlaq Ahmed <ahmedakhlaq009@gmail.com>
2026-01-08 01:51:27 +0500

    Swagger

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,4 @@
 import express from "express";
-import { ingestRouter } from "./ingest";
-import { transcriptRouter } from "./transcripts";
-import { analyticsRouter } from "./analytics";
-import { searchRouter } from "./search";
-import { graphRouter } from "./graph";
-
 
 import type MessageResponse from "../interfaces/message-response.js";
 
@@ -19,11 +13,5 @@ router.get<object, MessageResponse>("/", (req, res) => {
 });
 
 router.use("/emojis", emojis);
-router.use("/ingest", ingestRouter);
-router.use("/transcripts", transcriptRouter);
-router.use("/analytics", analyticsRouter);
-router.use("/search", searchRouter);
-router.use("/graph", graphRouter);
-
 
 export default router;
